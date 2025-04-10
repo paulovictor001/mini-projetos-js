8cea02a853be2e59fe5ca87f86c033711e416f8e
paulo victor <paulo20victor06@gmail.com>
2025-04-09 21:32:04 -0300

    ajuste

--- codigo.js
+++ codigo.js
@@ -1,13 +1,22 @@
 function calcular_imc(){
 
     var nome = document.getElementById("nome").value
-    var peso = parseFloat(document.getElementById("peso").value)
-    var altura = parseFloat(document.getElementById("altura").value)
+    var peso = document.getElementById("peso").value
+    var altura = document.getElementById("altura").value
     var resp = document.getElementById("resposta")
-
-    var imc = peso/altura
     
-    resp.innerText='óla '+ nome + ', seu  imc é de '+imc
+
+    if (peso.value !== '' || altura.value !== ''){
+        resp.innerText='preencha todos os campos'
+    }
+    else{
+        var imc = Math.round(peso/altura)
+        resp.innerText='óla '+ nome + ', seu  imc é de '+imc
+    }
+
+    
+    
+    
 
 
 }
